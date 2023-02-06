d91be6b0a4ff6f1df13960620f2c80d560b66e1b
IvanMV <johnmov@mail.ru>
2023-02-06 20:43:40 +0500

    Добавлен Navbar
    modified:   .backend/apis/general_pages/route_homepage.py
    modified:   .backend/main.py
    modified:   .backend/templates/components/navbar.html
    modified:   .backend/templates/shared/base.html

--- .backend/apis/general_pages/route_homepage.py
+++ .backend/apis/general_pages/route_homepage.py
@@ -3,11 +3,13 @@ from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-
+# создали объект шаблона Jinja2 и связали его с папкой шаблонов
 templates = Jinja2Templates(directory="templates")
+
+# создали экземпляр объекта APIRouter, его надо добавить в main
 general_pages_router = APIRouter()
 
-
+# создали функцию обработки запроса главной страницы, которая возвращает шаблон и запрос в виде словаря
 @general_pages_router.get("/")
 async def home(request: Request):
     return templates.TemplateResponse("general_pages/homepage.html",{"request":request})
--- .backend/main.py
+++ .backend/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
 
+# функция подключения APIRouter
 def include_router(app):
     app.include_router(general_pages_router)
-    
+
+# создание приложения и подключения APIRouter    
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
