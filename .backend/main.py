4ea40fc7c7e8b2de58bd38f2bf1f3ce1e0030162
IvanMV <johnmov@mail.ru>
2023-02-06 21:12:13 +0500

    Добавлена поддержка Static Files
    modified:   .backend/main.py
    modified:   .backend/requirements.txt
    new file:   .backend/static/images/logo.png
    modified:   .backend/templates/components/navbar.html
    modified:   .gitignore

--- .backend/main.py
+++ .backend/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
 
 # функция подключения APIRouter
 def include_router(app):
     app.include_router(general_pages_router)
+    
+# задаем путь и имя для статичных файлов нашего приложения
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # создание приложения и подключения APIRouter    
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
+    configure_static(app)
     return app
     
 app = start_application()
