61dfd9763e8b8f7fefcae0e9218118c7f6404f39
IvanMV <johnmov@mail.ru>
2023-02-08 16:24:03 +0500

    Добавлено создание пользователей

--- .backend/schemas/users.py
+++ .backend/schemas/users.py
@@ -6,3 +6,12 @@ class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+# данные для ответа после создания пользователя
+class ShowUser(BaseModel):
+    username : str
+    email : EmailStr
+    is_active : bool
+
+    class Config():  #tells pydantic to convert even non dict obj to json
+        orm_mode = True
