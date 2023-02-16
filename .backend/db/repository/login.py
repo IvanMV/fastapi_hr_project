4bed210b9425b10855527e809d70dc6fd04a1de7
IvanMV <johnmov@mail.ru>
2023-02-17 02:52:55 +0500

    Добавлена Авторизация

--- .backend/db/repository/login.py
+++ .backend/db/repository/login.py
@@ -1,9 +1,7 @@
+from db.models.users import User
 from sqlalchemy.orm import Session
 
-from db.models.users import User
 
-
-def get_user(username:str, db: Session):
+def get_user(username: str, db: Session):
     user = db.query(User).filter(User.email == username).first()
     return user
-
